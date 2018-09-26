8582c9a15a7e08f442eb12a6fd1766141eadcbe3
bartvanbossche <bvb@odoo.com>
2018-09-26 11:18:54 +0200

    [FIX] fix radius not increasing

--- pogom/app.py
+++ pogom/app.py
@@ -715,6 +715,11 @@ class Pogom(Flask):
                 currentlatitude -= stepsize
                 direction = "R"
                 currentlongitude += stepsize
+                if abs(currentlongitude - centerlongitude) < stepsize:
+                    direction = "U"
+                    currentlatitude += stepsize
+                    radius += 1
+                    step = 0
         elif direction == "R":
             currentlongitude += stepsize
             if currentlongitude > centerlongitude + radius * stepsize:
